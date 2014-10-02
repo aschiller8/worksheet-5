ab619b3eb99548e60b8ad47226edc2ae21fb2228
Aaron and Brandon <aschiller8@hotmail.com>
2014-10-02 09:50:16 -0400

    added coin flip function

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 /*global console*/ // This line tells jsLint that you're allowed to use console.log for printing.
 
+function minimum_of_two(first_number, second_number) {
+  var minimum;
+  if (first_number < second_number) {
+    minimum = first_number;
+  } 
+  else {
+    minimum = second_number;
+  }
+  return minimum;
+}
+
+function coin_flip() {
+  var heads_or_tails;
+  if (Math.random() < 0.5) {
+    heads_or_tails = 'heads';
+  } 
+  else {
+    heads_or_tails = 'tails';
+  }
+  return heads_or_tails;
+}
+
+
